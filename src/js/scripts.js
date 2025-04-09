b9e47c3f03f1c36fc90db1def98abd329243f72e
anniew01 <58456199+anniew01@users.noreply.github.com>
2025-04-08 21:35:13 -0400

    update

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,3 +5,15 @@
 // - Do not use onclick - use addEventListener instead
 // - Run npm run test regularly to check autograding
 // - You'll need to link this file to your HTML :)
+
+const startBtn = document.querySelector(".home-btn");
+
+function goToNext(){
+    const thisSection = this.closest(".homepage");
+    const nextSection=document.querySelector(".quiz-1");
+
+    thisSection.classList.toggle("show");
+    nextSection.classList.toggle("show");
+}
+
+startBtn.addEventListener("click", goToNext);
